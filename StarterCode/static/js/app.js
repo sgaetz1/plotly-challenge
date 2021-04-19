f6effcab0b61a3780723f4b58bcdb3f3f45f9fb5
Steven Gaetz <gaetz025@umn.edu>
2021-04-19 18:04:41 -0500

    populated dropdown menu

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,8 +1,17 @@
+// make sure file loaded
 console.log("app.js loaded");
 
-d3.json("../data/samples.json").then(function(samplesData){
+// get the samples data and print it to the console
+d3.json("../data/samples.json").then(function(sampleData){
     
-    console.log(samplesData);
+    console.log(sampleData);
 
+    var selector = d3.select("#selDataset");
 
-})
+    var sampleNames = sampleData.names;
+
+    sampleNames.forEach(sampleId => {
+        selector.append("option").text(sampleId).property("value",sampleId);
+    });
+
+});
