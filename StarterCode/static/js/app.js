a5b7bd43a86c5f98c5728cbc2325747ec5f7b3c9
Steven Gaetz <gaetz025@umn.edu>
2021-04-21 23:03:47 -0500

    bar graph working

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,9 +1,40 @@
 // make sure file loaded
 console.log("app.js loaded");
 
-
+// some of this code is from Dom in office hours
 function drawBargraph(sampleId) {
     console.log(`drawBargraph(${sampleId})`);
+
+    d3.json("../data/samples.json").then(data => {
+        // console.log(data);
+
+        var samples = data.samples;
+        var resultArray = samples.filter(samp => samp.id == sampleId);
+        var result = resultArray[0];
+        
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`).reverse();
+
+        var barData = {
+            x: sample_values.slice(0,10).reverse(),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0,10).reverse(),
+            orientation: "h"
+        }
+
+        var data = [barData];
+
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found"
+        }
+
+        Plotly.newPlot("bar", data, barLayout)
+
+    })
 }
 
 function drawBubblechart(sampleId) {
